2e3126f45db09adace38f5d3c665c61c78ce0fe1
JayPonsford <jayponsford@hotmail.com>
2022-11-03 11:19:05 +0000

    First rendition

---
+++ script.js
@@ -0,0 +1,11 @@
+//Iterate through card and append mousemove event listener
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
